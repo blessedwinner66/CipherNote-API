3ac31e6332b15864f3c1a56f254e4a69e3fd4bfb
Winner <blessedwinner@gmail.com>
2025-06-30 20:07:55 +0200

    Added findNote, updateNote and deleteNote functionalities

--- controllers/note.controller.js
+++ controllers/note.controller.js
@@ -11,3 +11,50 @@ module.exports.createNote = async(req,res)=>{
       console.log(e)
    }
 }
+
+module.exports.viewNotes = async(req,res)=>{
+  try{
+    const note = await Note.find()
+    res.status(200).json(note)
+  }
+   catch(e){
+      res.status(400).json({Error:e.message})
+      console.log(e)
+   }
+}
+
+module.exports.findNote = async(req,res)=>{
+  try{
+    const {id} = req.params
+    const note = await Note.findById(id,req.body,{new:true})
+    res.status(200).json(note)
+  }
+  catch(e){
+    res.status(400).json({Error:e.message})
+      console.log(e)
+  }
+}
+
+module.exports.updateNote = async(req,res)=>{
+  try{
+    const {id} = req.params
+    const note = await Note.findByIdAndUpdate(id)
+    res.status(200).json(note)
+  }
+  catch(e){
+    res.status(400).json({Error:e.message})
+    console.log(e)
+  }
+}
+
+module.exports.deleteNote = async(req,res)=>{
+  try{
+    const {id} = req.params
+    const note = await Note.findByIdAndDelete(id)
+    res.status(200).json({message:"Notes deleted successfully"})
+  }
+  catch(e){
+       res.status(400).json({Error:e.message})
+      console.log(e)
+  }
+}
--- routes/note.route.js
+++ routes/note.route.js
@@ -4,5 +4,9 @@ const requireAuth = require('../middleware/authMiddleware')
 const noteController = require('../controllers/note.controller')
 
 router.post('/notes/create',requireAuth,noteController.createNote)
+router.get('/notes',requireAuth,noteController.viewNotes)
+router.get('/notes/:id',requireAuth,noteController.findNote)
+router.put('/notes/:id',requireAuth,noteController.updateNote)
+router.delete('/notes/:id',requireAuth,noteController.deleteNote)
 
 module.exports = router
